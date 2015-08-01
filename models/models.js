a42f48b5a3ae1ce1f6233bbc58f33b1ad76ce399
David Marcos <dvironsav@gmail.com>
2015-08-01 14:32:10 +0200

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -56,7 +56,9 @@ sequelize.sync().then(function() {
         Quiz.count().then(function (count){
           if(count === 0) {   // la tabla se inicializa solo si está vacía
             Quiz.create(
-               {pregunta: 'Capital de Italia',   respuesta: 'Roma'} )
+               {pregunta: 'Capital de Italia',   respuesta: 'Roma'} );
+            Quiz.create(
+             {pregunta: 'Capital de Portugal',   respuesta: 'Lisboa'})
             .then(function(){console.log('Base de datos inicializada')});
           };
         });
